85fa51394284bc4df019a4e818103d27207a428e
Vishal Dodiya <vdodiya03@gmail.com>
2020-01-25 00:44:10 -0700

    initialize project

---
+++ src/components/container.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "../includes/container.scss";
+
+const Container = () => {
+    return(
+        <div className="container"></div>
+    );
+}
+
+export default Container;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import {render} from "react-dom";
+
+import Container from "./components/container";
+
+render(
+    <Container></Container>,
+    document.getElementById("llama-power")
+);
